3653aa6b4660afffd4228c3251ff870d686bdf20
Umar Khalilov <ERMASTER100@gmail.com>
2021-08-04 20:36:01 +0300

    Refactor js functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,41 +1,28 @@
 'use strict'
 
-let slideIndex = 1
+const slides = document.querySelectorAll('.slides');
+const dots = document.querySelectorAll('.demo');
+const captionText = document.getElementById('caption');
+let slideIndex = 1;
 
-showSlides(slideIndex)
-
-function plusSlides (num) {
-  showSlides((slideIndex += num))
+const showSlides = num => {
+    if (num > slides.length) {
+        slideIndex = 1;
+    }
+    if (num < 1) {
+        slideIndex = slides.length;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(' active', '');
+    }
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].className += ' active';
+    captionText.innerHTML = dots[slideIndex - 1].alt;
 }
 
-function currentSlide (num) {
-  showSlides((slideIndex = num))
-}
-
-function showSlides (num) {
-  const slides = document.querySelectorAll('.slides')
-
-  const dots = document.querySelectorAll('.demo')
-
-  const captionText = document.getElementById('caption')
-
-  if (num > slides.length) {
-    slideIndex = 1
-  }
-
-  if (num < 1) {
-    slideIndex = slides.length
-  }
-
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none'
-  }
-
-  for (let i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(' active', '')
-  }
-
-  slides[slideIndex - 1].style.display = 'block'
-  dots[slideIndex - 1].className += ' active'
-  captionText.innerHTML = dots[slideIndex - 1].alt
-}
+showSlides(slideIndex);
+const plusSlides = num => showSlides(slideIndex += num);
+const currentSlide = num => showSlides(slideIndex = num);
