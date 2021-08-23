e49a4db3a13760fd5ae655a7ade592531a963660
Umar Khalilov <ERMASTER100@gmail.com>
2021-08-23 23:51:48 +0300

    Add part of task

--- task_from_taras/six.js
+++ task_from_taras/six.js
@@ -1,15 +1,37 @@
 function A(matrix) {
     this.matrix = matrix;
 
-    function find(number) {
+    this.find = value => {
+        for (let i = 0; i < this.matrix.length; i++) {
+            let j = this.matrix[i].indexOf(value);
+            if (j !== -1) {
+                return [i, j];
+            }
+        }
+        return null;
     }
 
-    function get(location) {
+    this.get = location => {
+        for (let i = location[0]; ;) {
+            if (this.matrix[i][location[1]]) {
+                return this.matrix[i][location[1]];
+            }
+            return null;
+        }
     }
 
-    function zip() {
+    this.zip = function () {
     }
 
-    function unzip() {
+    this.unzip = function () {
     }
 }
+
+const arr = [[0, 5, 2],
+    [3, 4, 5]];
+
+const instance = new A(arr);
+
+console.log(instance.find(5));
+
+console.log(instance.get([1, 2]))
